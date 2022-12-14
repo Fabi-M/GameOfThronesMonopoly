887d7b14d88b7dda859c54ef04d0fd40e2df10e5
DESKTOP-UOIM227\renea <ra@markatus.de>
2022-12-14 13:06:54 +0100

    ADD buttons

--- private/Test/Controller/Renetestcontroller.php
+++ private/Test/Controller/Renetestcontroller.php
@@ -7,8 +7,6 @@ use GameOfThronesMonopoly\Core\Controller\BaseController;
 class Renetestcontroller extends BaseController
 {
     public function TestAction(){
-        var_dump($this->sessionId);
-        var_dump(__DIR__.'\..\..\..\public\img\Playfield.jpg');
 
         echo $this->twig->render("Game/Views/Game.html.twig",
             [
