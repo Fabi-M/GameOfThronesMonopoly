1f9a8e43eaf4e110c0cfd71e6e6f7e11961e77b0
SelinaStöcklein <selina.stoecklein18@gmail.com>
2022-12-20 09:15:10 +0100

    Allgemeines PopUp

--- public/js/Dice.js
+++ public/js/Dice.js
@@ -1,13 +1,19 @@
 class Dice {
     constructor() {
         let events = new Events();
-        events.addEvent('click', $('#wuerfeln'), this.throwDices, {"this": this});
+        events.addEvent('click', $('.dice'), this.throwDices, {"this": this});
     }
 
     /**
      * @author Selina Stöcklein & Christian Teubner
      */
     throwDices(event, data) {
+        let errorDialog = new ModalDialog(); // build ModalDialog
+        
+        errorDialog.Title = "<h3>Fehler beim Hochladen</h3>"; // add a title
+        errorDialog.Body = "<p>Leider ist ein Fehler beim Hochladen deiner Daten aufgetreten. Bitte lade die Seite neu und versuche es nochmal.</p>";
+        errorDialog.showDialog();
+        return;
         let that = data['this'];
         //TODO 15.12.2022 Selina: soll je nach action Move oder Prison an URL anhängen "data-action='Move'"
         //TODO 15.12.2022 Selina: Alle buttons die wie Dice interagieren sollen brauchen die Klasse "dice"
