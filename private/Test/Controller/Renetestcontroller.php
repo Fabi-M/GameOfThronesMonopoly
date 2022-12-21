8404a6825b5c3ae9b9d286ef67005b25ce6211a6
ChristianTeubner <chrisi.teubner@gmail.com>
2022-12-21 08:16:30 +0100

    Changed Dice

--- private/Test/Controller/Renetestcontroller.php
+++ private/Test/Controller/Renetestcontroller.php
@@ -15,8 +15,10 @@ class Renetestcontroller extends BaseController
         $this->scriptCollector->addBottom('/js/Core/Events.js');
         $this->scriptCollector->addBottom('/js/Figure.js');
         $this->scriptCollector->addBottom('/js/FigureService.js');
+        $this->scriptCollector->addBottom('/js/PlayField.js');
         $this->scriptCollector->addBottom('/js/PlayFieldService.js');
         $this->scriptCollector->addBottom('/js/Dice.js');
+        $this->scriptCollector->addBottom('/js/Round.js');
         $this->styleSheetCollector->addBottom(
             '/node_modules/bootstrap/dist/css/bootstrap.min.css'
         );
