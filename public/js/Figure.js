32029528d853ac8ecd9438874f267cb069b8ce16
SelinaStöcklein <selina.stoecklein18@gmail.com>
2022-12-22 11:50:05 +0100

    ADDED 055: Toast für Würfeln und Miete

--- public/js/Figure.js
+++ public/js/Figure.js
@@ -20,12 +20,33 @@ class Figure {
      */
     payRent(targetPlayFieldId) {
         let url = BASEPATH + '/Street/Rent/Pay';
-        let request = new Ajax(url, {"playFieldId": targetPlayFieldId}, this.showResult, false);
+        let request = new Ajax(
+            url,
+            {"playFieldId": targetPlayFieldId},
+            this.showResult,
+            {'this': this});
         request.execute();
     }
 
-    showResult(result) {
+    toastRent(result) {
+        let resultObj = JSON.parse(result);
+        console.log(resultObj);
+        if (resultObj['payedRent'] > 0) {
+            let toast = new Toast();
+            toast.Heading = 'Es ist Miete zu zahlen!';
+            let body = 'Du hast <strong>' + resultObj['payedRent'] + '</strong> gezahlt.';
+            if (resultObj['isGameOver'] === 'true') {
+                body += ' Du bist pleite gegangen... GAMEOVER';
+            }
+            toast.Body = body;
+            toast.show();
+        }
+    }
+
+    showResult(result, data) {
         //TODO 21.12.2022 Selina: anzeigen ob rent gezahlt wurde, money aktualisieren, gamoover anzeigen
         console.log(result);
+        console.log(data);
+        data['this'].toastRent(result);
     }
 }
